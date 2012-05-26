a4a575a0262221f2f0efa5382379e5551469f3db
Mark Huot <mark@markhuot.com>
2012-05-26 01:56:00 -0400

    simplifying things

--- MY_Controller.php
+++ MY_Controller.php
@@ -1,11 +1,18 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class MY_Controller extends CI_Controller {
+
 	/**
-	 * $data
-	 * Data to be sent automatically to the view.
+	 * Headers to set when calling the constructor
 	 */
-	protected $data;
+	protected $content_type = 'text/html';
+	protected $cache_control = 'no-cache';
+
+	/**
+	 * The Layout and View to load after the controller function runs.
+	 */
+	protected $layout = 'application/layout';
+	protected $view;
 
 	/**
 	 * Our before and after filters. These allow us to avoid overriding the
@@ -15,49 +22,36 @@ class MY_Controller extends CI_Controller {
 	protected $after_filter = array();
 
 	/**
-	 * $formats
-	 * An array of respondable formats. This maps the request type to a
-	 * template and a template language, some examples included.
-	 */
-	private $formats = array(
-		//'html' => 'custom/view/file.php',
-		//'json' => 'custom/file.json'
-	);
-
-	/**
-	 * Constructor
-	 * Our custom init method to load in our template driver
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->driver('template');
-	}
-
-	/**
 	 * Remap
 	 * Overrides each request and allows us to automatically call the template
 	 * parser after the controller's method has run.
 	 */
-	public function _remap($method)
+	public function _remap($method, $params=array())
 	{
+		$this->view = strtolower(get_class($this)).'/'.$method);
+
+		$verb = strtolower($_SERVER['REQUEST_METHOD']);
+
 		// If the method doesn't exist bail out.
-		if (!method_exists($this, $method)) {
+		if (method_exists($this, "{$verb}_{$method}")) {
+			$method = "{$verb}_{$method}";
+		}
+		else if (method_exists($this, $method)) {
+			$method = $method;
+		}
+		else {
 			show_404();
 		}
 
-		// Run our :before_filter
-		$this->run_filter('before');
-
-		// Store the state of our controller prior to the method call
+		// Get the state of the controller before running any methods
 		$before = get_object_vars($this);
+
+		// Run our :before_filter
+		$this->run_filter('before', $params);
 
 		// Call the method
 		try {
-			call_user_func_array(
-				array($this, $method),
-				array_slice($this->uri->rsegments, 2)
-			);
+			$return = call_user_func_array(array($this, $method), $params);
 		}
 
 		// Catch any errors the method throws
@@ -65,80 +59,55 @@ class MY_Controller extends CI_Controller {
 			show_error($e->getMessage());
 		}
 
+		// Run our :after_filter
+		$this->run_filter('after', $params);
+
 		// Get the state of our controller after running the method
 		$after = get_object_vars($this);
 
-		// Figure out if anything changed. If it did then we'll want to add
-		// those changes into the data array unless their name begins with an
-		// underscore.
-		$diff = array_diff(array_keys($after), array_keys($before));
-		foreach ($diff as $key) {
-			if (substr($key, 0, 1) == '_') {
-				continue;
-			}
-			$this->data[$key] = $this->{$key};
+		// Figure out if anything changed. If it did then we'll want to make
+		// those changes available to the view
+		$vars = array_diff_key($after, $before);
+
+		// Set the headers now that the controller has had a chance to
+		// modify them.
+		if ($this->content_type) {
+			$this->output->set_content_type($this->content_type);
+		}
+		if ($this->cache_control) {
+			header("Cache-control: {$this->cache_control}");
+			header("Edge-control: {$this->cache_control}");
 		}
 
-		// Run our :after_filter
-		$this->run_filter('after');
-
-		// Simplify the class variable
-		$class = $this->router->class;
-
-		// Strip any HTTP verbs out of the method variable so we can get to
-		// the name of the view. Blog::get_posts would be a view of blog/posts
-		$method = preg_replace(
-			'/^(get|post|put|delete)_/',
-			'',
-			$this->router->method
-		);
-
-		// Get the format from the URI.
-		$format = $this->uri->format();
-
-		// Is a specific view defined for this format?
-		if (isset($this->formats[$format])) {
-			$view = $this->formats[$format];
-		}
-		
-		// No defined view, assemble the default view/template
-		else if($this->template->responds_to($format)) {
-			$view = "{$class}/{$method}.{$format}";
+		// If we have return data from the controller method stop here and just
+		// render that out.
+		if ($return) {
+			$this->output->set_output($return);
+			return;
 		}
 
-		// No suitable parser was defined/found for the format
-		else {
-			show_404();
+		// Get the state of the loader object before loading our view
+		$before = get_object_vars($this->load);
+
+		// Render the view out to a string.
+		if ($this->view) {
+			$output = $this->load->view($this->view, $vars, TRUE);
 		}
 
-		// Check for redirects
-		if (substr($view, 0, 1) == ':') {
-			$this->load->helper('url');
-			redirect(substr($view, 1));
+		// Get the state of the loader after running the view. This allows
+		// class variables set in a view to bubble up to the layout
+		$after = get_object_vars($this->load);
+		$view_vars = array_diff_key($after, $before);
+
+		// Merge our vars for inclusion into the layout
+		$vars = array_merge($vars, $view_vars, array('yield'=>$output));
+
+		// Render the layout
+		if ($this->layout) {
+			$output = $this->load->view('application/layout', $vars, TRUE);
 		}
 
-		// Setup some default data. We'll take the session flashdata, if it
-		// exists and push it onto the data passed to the template
-		if (isset($this->session) && !isset($this->data['flashdata'])) {
-			$this->data['flashdata'] = new stdClass;
-			foreach ($this->session->all_userdata() as $key => $value) {
-				if (preg_match('/^flash:old:(.*)$/', $key, $match)) {
-					$this->data['flashdata']->{$match[1]} = $value;
-				}
-			}
-		}
-
-		// Finally, render out the template; assuming the developer still
-		// wants us to.
-		$this->template->render($view, $this->data);
-
-		// Get the parser defined in the URL. This could be different from the
-		// parser used to generate the page. For example a JSON request could
-		// be driven by a `twig` template. In this case we'll want to pull the
-		// content type from `json` even though the template engine is `twig`.
-		$parser = $this->uri->format();
-		$content_type = $this->template->{$parser}->content_type;
-		$this->output->set_content_type($content_type);
+		$this->output->set_output($output);
 	}
 
 	/**
@@ -156,33 +125,7 @@ class MY_Controller extends CI_Controller {
 		}
 
 		foreach ($filter as $method) {
-			call_user_func_array(
-				array($this, $method),
-				array_slice($this->uri->rsegments, 2)
-			);
-		}
-	}
-
-	/**
-	 * Responds To
-	 * Set the templates to use in the response per format
-	 */
-	public function responds_to($formats, $empty_defaults=FALSE)
-	{
-		if ($empty_defaults) {
-			$this->formats = array();
-		}
-
-		foreach ($formats as $key => $value)
-		{
-			if (is_numeric($key))
-			{
-				$this->formats[$value] = FALSE;
-			}
-			else
-			{
-				$this->formats[$key] = $value;
-			}
+			call_user_func_array(array($this, $method), $params);
 		}
 	}
 }
