432ebb37573a2163ec958634f4dd2d71cac87c1e
Mark Huot <mark@markhuot.com>
2012-05-26 03:46:09 -0400

    adding helper for model

--- MY_Model.php
+++ MY_Model.php
@@ -86,3 +86,35 @@ class MY_Model extends CI_Model {
 	}
 
 }
+
+<?php
+
+if (!function_exists('array_merge_recursive_overwrite')) {
+	function array_merge_recursive_overwrite() {
+
+		if (func_num_args() < 2) {
+			trigger_error(__FUNCTION__ .' needs two or more array arguments', E_USER_WARNING);
+			return;
+		}
+		$arrays = func_get_args();
+		$merged = array();
+		while ($arrays) {
+			$array = array_shift($arrays);
+			if (!is_array($array)) {
+				trigger_error(__FUNCTION__ .' encountered a non array argument', E_USER_WARNING);
+				return;
+			}
+			if (!$array)
+				continue;
+			foreach ($array as $key => $value)
+				if (is_string($key))
+					if (is_array($value) && array_key_exists($key, $merged) && is_array($merged[$key]))
+						$merged[$key] = call_user_func(__FUNCTION__, $merged[$key], $value);
+					else
+						$merged[$key] = $value;
+				else
+					$merged[] = $value;
+		}
+		return $merged;
+	}
+}
